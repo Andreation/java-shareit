7ed6e6903482d39b38ecac80fdfbe4e24f2da1dd
Andreation <115004597+Andreation@users.noreply.github.com>
2023-06-30 04:12:25 +1000

    fix

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -1,13 +1,5 @@
 package ru.practicum.shareit.request.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import ru.practicum.shareit.user.model.User;
-
-import javax.persistence.*;
-
-
 public class ItemRequest {
 
 }
--- src/main/java/ru/practicum/shareit/request/model/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequestDto.java
@@ -1,7 +1,5 @@
 package ru.practicum.shareit.request.model;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * TODO Sprint add-item-requests.
