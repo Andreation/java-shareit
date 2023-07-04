a51fb267b9d25d4c8f602a3b471ade77d5e0f159
Andreation <115004597+Andreation@users.noreply.github.com>
2023-07-05 08:06:14 +1000

    tests

--- src/test/java/ru/practicum/shareit/item/controller/ItemControllerTest.java
+++ src/test/java/ru/practicum/shareit/item/controller/ItemControllerTest.java
@@ -15,7 +15,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 
-import static org.hamcrest.Matchers.hasSize;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
