16a5f1f61c847ac88db26368fe81b05977fa8c96
Andreation <115004597+Andreation@users.noreply.github.com>
2023-07-05 06:20:03 +1000

    some tests

---
+++ src/test/java/ru/practicum/shareit/booking/model/StateTest.java
@@ -0,0 +1,21 @@
+package ru.practicum.shareit.booking.model;
+
+import org.junit.jupiter.api.Test;
+import ru.practicum.shareit.exception.ExceptionForUnsupport;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StateTest {
+
+    @Test
+    void getState() {
+        String str = "ALL";
+        State state = State.getState(str);
+        assertEquals(State.ALL, state);
+        str = "CURRENT";
+        state = State.getState(str);
+        assertEquals(State.CURRENT, state);
+        assertThrows(ExceptionForUnsupport.class, () -> State.getState("asd"));
+
+    }
+}
