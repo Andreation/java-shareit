01acc907cbbe0153062acdfbaddce1182bd595df
Andreation <115004597+Andreation@users.noreply.github.com>
2023-07-04 10:20:57 +1000

    some tests

--- src/test/java/ru/practicum/shareit/item/repository/CommentRepositoryTest.java
+++ src/test/java/ru/practicum/shareit/item/repository/CommentRepositoryTest.java
@@ -28,40 +28,6 @@ class CommentRepositoryTest {
 
     @Test
     @DirtiesContext
-    void findAllByItemId() {
-        User user1 = new User(1L,"user1","user1@mail.ru");
-        userRepository.save(user1);
-
-        User user2 = new User(2L,"user2","user2@mail.ru");
-        userRepository.save(user2);
-
-        User user3 = new User(3L,"user3","user3@mail.ru");
-        userRepository.save(user3);
-
-        Item item1 = new Item(1L,"item1","itemDecription1",true);
-        item1.setOwner(userRepository.findById(1L).orElseThrow(() -> new NotFoundException("no found")));
-        itemRepository.save(item1);
-
-        Item item2 = new Item(2L,"item2","itemDecription2",true);
-        item2.setOwner(userRepository.findById(1L).orElseThrow(() -> new NotFoundException("no found")));
-        itemRepository.save(item2);
-
-        LocalDateTime date = LocalDateTime.now();
-        Comment comment1 = commentRepository.save(new Comment("comment1", item1,  user2, date));
-        Comment comment2 = commentRepository.save(new Comment("comment2", item2,  user3, date));
-        Comment comment3 = commentRepository.save(new Comment("comment3", item1,  user3, date));
-        Comment comment4 = commentRepository.save(new Comment("comment4", item2,  user2, date));
-
-        List<Comment> comments = commentRepository.findAllByItemIdIn(List.of(item1.getId(),item2.getId()));
-        assertEquals(comments.size(),4);
-        assertEquals(comments.get(0).getText(),comment1.getText());
-        assertEquals(comments.get(1).getText(),comment2.getText());
-        assertEquals(comments.get(2).getText(),comment3.getText());
-        assertEquals(comments.get(3).getText(),comment4.getText());
-    }
-
-    @Test
-    @DirtiesContext
     public void findAllByItemIdIn() {
         User user1 = new User(1L,"user1","user1@mail.ru");
         userRepository.save(user1);
