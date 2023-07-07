7c4fab70a24921319357f1df1ed24dbe89ebf0d7
Andreation <115004597+Andreation@users.noreply.github.com>
2023-07-08 03:55:53 +1000

    fix

--- gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -7,23 +7,54 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.validation.ConstraintViolationException;
-
-@RestControllerAdvice
 @Slf4j
+@RestControllerAdvice
 public class ErrorHandler {
 
-    @ExceptionHandler({MethodArgumentNotValidException.class, ConstraintViolationException.class})
+    @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleMethodArgumentNotValidError(final RuntimeException e) {
-        log.warn(e.getMessage());
-        return new ErrorResponse(e.getMessage());
+    public ErrorResponse handleValidationException(final ValidationException e) {
+        log.error("BAD_REQUEST : {}", e.getMessage());
+        String errorMessage = "ValidationException: " + e.getMessage();
+        return new ErrorResponse(errorMessage);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus (HttpStatus.BAD_REQUEST)
+    public ErrorResponse validateArgumentException(final MethodArgumentNotValidException e) {
+        log.error("Validation error: : {}", e.getMessage());
+        String errorMessage = "Validation error: " + e.getMessage();
+        return new ErrorResponse(errorMessage);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus (HttpStatus.FORBIDDEN)
+    public ErrorResponse handleForbiddenException(final ForbiddenException e) {
+        log.error("ForbiddenException: : {}", e.getMessage());
+        String errorMessage = "ForbiddenException: " + e.getMessage();
+        return new ErrorResponse(errorMessage);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(final NotFoundException e) {
+        log.error("NotFoundException: : {}", e.getMessage());
+        String errorMessage = "NotFoundException: " + e.getMessage();
+        return new ErrorResponse(errorMessage);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus (HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleExceptionForUnsupport(final ExceptionForUnsupport e) {
+        log.error("INTERNAL_SERVER_ERROR: : {}", e.getMessage());
+        String strError = e.getMessage();
+        return new ErrorResponse(strError);
     }
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleThrowable(final Throwable e) {
-        log.warn(e.getMessage());
-        return new ErrorResponse(e.getMessage());
+        String strError = e.getMessage();
+        return new ErrorResponse(strError);
     }
 }
--- gateway/src/main/java/ru/practicum/shareit/exception/ErrorResponse.java
+++ gateway/src/main/java/ru/practicum/shareit/exception/ErrorResponse.java
@@ -1,13 +1,10 @@
 package ru.practicum.shareit.exception;
 
-import lombok.EqualsAndHashCode;
-
-@EqualsAndHashCode
 public class ErrorResponse {
     private final String error;
 
-    public ErrorResponse(String error) {
-        this.error = error;
+    public ErrorResponse(String errorMessage) {
+        this.error = errorMessage;
     }
 
     public String getError() {
